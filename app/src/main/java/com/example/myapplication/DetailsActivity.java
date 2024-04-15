db0531c4ff25f3637b7870a9c7b16e5dbd9d6919
negarahani <106038839+negarahani@users.noreply.github.com>
2024-04-15 02:12:03 -0700

    Apr 15 first

--- app/src/main/java/com/example/myapplication/DetailsActivity.java
+++ app/src/main/java/com/example/myapplication/DetailsActivity.java
@@ -1,12 +1,20 @@
 package com.example.myapplication;
 
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -32,10 +40,13 @@ import com.google.gson.reflect.TypeToken;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
+import android.util.DisplayMetrics;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -43,10 +54,15 @@ public class DetailsActivity extends AppCompatActivity {
     private List<FavoriteStock> myFavoriteStocks = new ArrayList<>(); //you may change this later!
     private String curTicker;
     private String curCompanyName;
+    private Double curPrice;
     private boolean isInFavorites;
 
     private static final String PREFS_NAME = "MyFavoriteStocksPrefs";
     private static final String KEY_FAVORITE_STOCKS = "favoriteStocks";
+    private Button tradeButton;
+
+    Integer quantityToBuy = 0;
+    Double totalCostToBuy = 0.00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,7 +115,88 @@ public class DetailsActivity extends AppCompatActivity {
                 isInFavorites = !isInFavorites;
             }
         });
+
+        /*********** set up the dialogue button event listener *****************/
+
+            tradeButton = (Button) findViewById(R.id.tradeButton);
+
+            // add button listener
+            tradeButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+
+                    // custom dialog
+                    final Dialog dialog = new Dialog(DetailsActivity.this);
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                    dialog.setContentView(R.layout.trade_dialogue);
+
+                    //setting the dialogue width to 90% of the screen
+                    DisplayMetrics displayMetrics = new DisplayMetrics();
+                    getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                    int dialogWidth = (int) (displayMetrics.widthPixels * 0.90);
+                    WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+                    layoutParams.copyFrom(dialog.getWindow().getAttributes());
+                    layoutParams.width = dialogWidth;
+                    dialog.getWindow().setAttributes(layoutParams);
+
+                    // set the custom dialog components - text, image and button
+                    TextView dialogueHeader = dialog.findViewById(R.id.dialogueHeader);
+                    dialogueHeader.setText("Trade " + curCompanyName + " Shares");
+
+                    EditText editText = dialog.findViewById(R.id.editText);
+                    TextView tradeInputGuideText = dialog.findViewById(R.id.tradeInputGuideText);
+                    tradeInputGuideText.setText(quantityToBuy + "*$" + curPrice + "/share = " + totalCostToBuy);
+
+                    TextView tradeBalanceText = dialog.findViewById(R.id.tradeBalanceText);
+                    tradeBalanceText.setText(" to buy " + curTicker);
+
+                    editText.addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence seq, int start, int count, int after) {
+                            // not needed
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence seq, int start, int before, int count) {
+                            //Updating tradeInputListener TextView with the text entered by the user
+
+                            if (!seq.toString().isEmpty()) {
+                                quantityToBuy = Integer.parseInt(seq.toString());
+                                totalCostToBuy = quantityToBuy * curPrice;
+                                String formattedTotalCostToBuy = String.format("%.2f", totalCostToBuy);
+                                tradeInputGuideText.setText(quantityToBuy + "*$" + curPrice + "/share = $" + formattedTotalCostToBuy);
+                            } else {
+                                // If the input is empty, set quantityToBuy and totalCostToBuy to 0
+                                quantityToBuy = 0;
+                                totalCostToBuy = 0.0;
+                                tradeInputGuideText.setText(quantityToBuy + "*$" + curPrice + "/share = $" + totalCostToBuy);
+                            }
+
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable e) {
+                            // not needed
+                        }
+                    });
+
+
+                    Button buyButton = (Button) dialog.findViewById(R.id.buyButton);
+                    // if button is clicked, close the custom dialog
+                    buyButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    dialog.show();
+                }
+            });
     }
+
 
     private void initStarButton(String tickerSymbol){
         myFavoriteStocks = loadFavoriteStocks();
@@ -181,8 +278,8 @@ public class DetailsActivity extends AppCompatActivity {
         String quoteUrl = BASE_URL + "/search-quote/" + tickerSymbol;
 
 
-        TextView currentPrice = findViewById(R.id.current_price);
-        TextView change = findViewById(R.id.change);
+        TextView currentPriceText = findViewById(R.id.current_price);
+        TextView changeText = findViewById(R.id.change);
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
@@ -195,17 +292,19 @@ public class DetailsActivity extends AppCompatActivity {
                         try {
                             // Parsing the JSON response
                             JSONObject jsonObject = new JSONObject(response);
-                            double curPrice = jsonObject.getDouble("c");
+                            double currentPrice = jsonObject.getDouble("c");
                             double changeVal = jsonObject.getDouble("d");
                             double changePercent = jsonObject.getDouble("dp");
 
+                            curPrice = currentPrice; //set the class attribute to the obtained current price
+
                             // Convert double values to strings
-                            String curPriceStr = String.valueOf(curPrice);
+                            String curPriceStr = String.valueOf(currentPrice);
                             String changeValStr = String.valueOf(changeVal);
                             String changePercentStr = String.valueOf(changePercent);
 
-                            currentPrice.setText("$" + curPriceStr);
-                            change.setText("$" + changeValStr + " (" + changePercentStr + "%)");
+                            currentPriceText.setText("$" + curPriceStr);
+                            changeText.setText("$" + changeValStr + " (" + changePercentStr + "%)");
 
                         } catch (JSONException e) {
                             // JSON parsing error
